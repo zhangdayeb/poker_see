4ae8507cc72f2aa404f81ab19cca3ce066c15dfe
zhangdayeb <you@example.com>
2025-05-28 14:37:34 +0800

    'up'

---
+++ src/processors/engines/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+识别引擎包
+Recognition engines package
+"""
+
+__all__ = [
+    'BaseEngine',
+    'YOLOEngine', 
+    'OCREngine',
+    'HybridEngine',
+    'OpenCVEngine'
+]
+
+# 版本信息
+__version__ = "1.0.0"
---
+++ src/processors/utils/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+识别工具包
+Recognition utilities package
+"""
+
+__all__ = [
+    'ImagePreprocessor',
+    'ResultNormalizer', 
+    'RecognitionConfigLoader'
+]
+
+# 版本信息
+__version__ = "1.0.0"
